b4725f0320d40ed1104b24b978755edd66643da0
JasperMunene <jaspermunene2022@gmail.com>
2024-11-04 10:03:25 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -8,16 +8,25 @@ import SortSelector from './SortSelector';
 function App() {
     const [option, setOption] = useState('none');
     const [hogList, setHogList] = useState(hogs);
+    const [showGreased, setShowGreased] = useState(false);
 
     const handleSortChange = (e) => {
         setOption(e.target.value);
+    };
+
+    const handleGreasedChange = (e) => {
+        setShowGreased(e.target.checked);
     };
 
     const addHog = (newHog) => {
         setHogList([...hogList, newHog]);
     };
 
-    const sortedHogs = [...hogList].sort((a, b) => {
+    const filteredHogs = hogList.filter(hog => {
+        return showGreased ? hog.greased : true;
+    });
+
+    const sortedHogs = [...filteredHogs].sort((a, b) => {
         if (option === 'name') {
             return a.name.localeCompare(b.name);
         } else if (option === 'weight') {
@@ -30,21 +39,28 @@ function App() {
         <div className="App">
             <Nav />
             <SortSelector option={option} onSortChange={handleSortChange} />
-            <div className="ui grid container">
-            {sortedHogs.map((hog) => (
-                <Tile
-                    key={hog.name}
-                    name={hog.name}
-                    specialty={hog.specialty}
-                    greased={hog.greased}
-                    weight={hog.weight}
-                    highest={hog['highest medal achieved']}
-                    image={hog.image}
+            <div className="filter-options">
+                <label htmlFor="greased-filter">Show Greased Only</label>
+                <input
+                    type="checkbox"
+                    id="greased-filter"
+                    checked={showGreased}
+                    onChange={handleGreasedChange}
                 />
-            ))}
             </div>
-            
-
+            <div className="ui grid container">
+                {sortedHogs.map((hog) => (
+                    <Tile
+                        key={hog.name}
+                        name={hog.name}
+                        specialty={hog.specialty}
+                        greased={hog.greased}
+                        weight={hog.weight}
+                        highest={hog['highest medal achieved']}
+                        image={hog.image}
+                    />
+                ))}
+            </div>
             <HogForm addHog={addHog} />
         </div>
     );
